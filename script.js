0b4c7cb1fc44b57e2b1e117ae5eba2a0de4f50d8
Ayush Vekariya <102176449+AyushV75@users.noreply.github.com>
2024-01-22 13:54:22 -0500

    Updated the javascript and added CSS for style

--- script.js
+++ script.js
@@ -1,42 +1,43 @@
-const taskForm=document.getElementById("task-form");
-const taskList=document.getElementById("task-list");
+const taskForm = document.getElementById("task-form");
+const taskList = document.getElementById("task-list");
 
 // Counter variable to track the task index
-let taskIndex=1;
+let taskIndex = 1;
 // console.log(taskForm,taskList);
 
+taskForm.addEventListener("submit", function (event) {
+  event.preventDefault();
 
-taskForm.addEventListener("submit", function (event){
-    event.preventDefault();
+  const taskInput = document.getElementById("task-input");
+  const taskText = taskInput.value.trim();
 
-    const taskInput= document.getElementById("task-input");
-    const taskText = taskInput.value.trim();
+  // console.log(taskText);
+  if (taskText === "") {
+    alert("Please enter a valid Task!");
+    return;
+  } else if (taskText !== "") {
+    // Create a new task item
 
-    // console.log(taskText);
-    if(taskText ===""){
-        alert("Please enter a valid Task!");
-        return;
-        }
-    else if(taskText !==""){
-        // Create a new task item
+    const taskItem = document.createElement("li");
+    taskItem.classList.add("task-item");
+    taskItem.textContent = `${taskIndex}- ${taskText}`;
+    taskItem.addEventListener("click", function () {
+      console.log("completed");
+      this.classList.toggle("completed");
+    });
+    // Add another click event listener to remove the completed task
+    taskItem.addEventListener("click", function (event) {
+      if (event.target.classList.contains("completed")) {
+        // Remove the completed task item
+        taskList.removeChild(taskItem);
+      }
+    });
 
-        const taskItem= document.createElement("li");
-        taskItem.classList.add("task-item");
-        taskItem.textContent= `${taskIndex}- ${taskText}`;
-        taskItem.addEventListener("click", function(){
-            console.log("completed");
-            this.classList.toggle("completed");
+    // Append the task item to the task list
+    taskList.appendChild(taskItem);
 
-        });
-
-
-
-
-        // Append the task item to the task list
-        taskList.appendChild(taskItem);
-
-        // increment the task index
-        taskIndex++;
-        taskInput.value= "";
-    }
+    // increment the task index
+    taskIndex++;
+    taskInput.value = "";
+  }
 });
